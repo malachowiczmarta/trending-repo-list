cc9095d133b980b519bd5935ed1b4b53945d3057
Marta Małachowicz <marta@exefs-macbook-pro.home>
2021-02-14 12:29:05 +0100

    add id to radio btn

--- src/components/date/Date.tsx
+++ src/components/date/Date.tsx
@@ -18,13 +18,13 @@ const Date = observer(({toggleDropdown}: DateProps) => {
     <div className="date-dd-wrapper">
       <form className="date-container">
         <label className="input-container" htmlFor="daily">daily
-          <input type="radio" checked={store.dateRange === dateRangeOption.DAILY} value="daily" onChange={(event) => handleDateChange(event)} />
+          <input id="daily" type="radio" checked={store.dateRange === dateRangeOption.DAILY} value="daily" onChange={(event) => handleDateChange(event)} />
         </label>
         <label className="input-container" htmlFor="weekly">weekly
-          <input type="radio" checked={store.dateRange === dateRangeOption.WEEKLY} value="weekly" onChange={(event) => handleDateChange(event)} />
+          <input id="weekly" type="radio" checked={store.dateRange === dateRangeOption.WEEKLY} value="weekly" onChange={(event) => handleDateChange(event)} />
         </label>
         <label className="input-container" htmlFor="monthly">monthly
-          <input type="radio" checked={store.dateRange === dateRangeOption.MONTHLY} value="monthly" onChange={(event) => handleDateChange(event)} />
+          <input id="monthly" type="radio" checked={store.dateRange === dateRangeOption.MONTHLY} value="monthly" onChange={(event) => handleDateChange(event)} />
         </label>
       </form>
     </div>
